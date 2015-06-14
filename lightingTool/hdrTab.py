2205642773b12318d9ac4125b015340672388508
jacobgonz <jacobgonzp@gmail.com>
2015-06-14 16:33:19 +0100

    explore folder linux fix

--- lightingTool/hdrTab.py
+++ lightingTool/hdrTab.py
@@ -303,7 +303,7 @@ class TabContent():
         elif sys.platform == "win32":
             os.system('start %s' % folderPath)
         elif sys.platform == "linux2":
-            subprocess.Popen(['xdg-open', folderPath])
+            os.system('nautilus %s' % folderPath)
         else:
             print "Explore folder not implemented for this OS"
 
